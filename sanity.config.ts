3e076484b60e58f2be2b5deb576a269d2e2ce269
Bruno Alla <alla.brunoo@gmail.com>
2024-08-17 13:28:18 +0200

    Replace deprecated deskTool by structure

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'sanity'
-import { deskTool } from 'sanity/desk'
+import { structureTool } from 'sanity/structure'
 import { media } from 'sanity-plugin-media'
 import { frFRLocale } from '@sanity/locale-fr-fr'
 import schemas from './schemas/schema'
@@ -8,7 +8,7 @@ export default defineConfig({
   title: 'Les Capsules',
   projectId: '9xyjnlvq',
   dataset: 'production',
-  plugins: [deskTool(), media(), frFRLocale()],
+  plugins: [structureTool(), media(), frFRLocale()],
 
   schema: {
     types: schemas,
