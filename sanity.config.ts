3d96d2985300818968af70c187d1183f62a94474
Bruno Alla <alla.brunoo@gmail.com>
2024-08-17 13:25:50 +0200

    Localize studio to French

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { media } from 'sanity-plugin-media'
+import { frFRLocale } from '@sanity/locale-fr-fr'
 import schemas from './schemas/schema'
 
 export default defineConfig({
   title: 'Les Capsules',
   projectId: '9xyjnlvq',
   dataset: 'production',
-  plugins: [deskTool(), media()],
+  plugins: [deskTool(), media(), frFRLocale()],
 
   schema: {
     types: schemas,
